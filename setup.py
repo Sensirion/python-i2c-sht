072be89b24b089f738a8f5465296626cb5c9426b
Builder <builder@sensirion.com>
2021-03-19 13:34:56 +0100

    Remove build dependency "pytest-runner" from setup.py
    The build dependency "pytest-runner" (specified with "setup_requires"
    in setup.py) causes pytest-runner to be installed on every invocation of
    "python setup.py <command>". This no longer works on Python versions
    older than 3.6 because pytest-runner has a dependency to the latest
    version of setuptools_scm which recently dropped support for Python
    older than 3.6. Now the CI of many Python projects is broken because of
    this incompatibility.
    
    Since usually we don't run tests with "python setup.py test", we
    actually don't need this dependency anyway. So it should be safe to
    simply remove the dependency. Running tests with the "pytest" command
    will still work as usual.

--- setup.py
+++ setup.py
@@ -35,9 +35,6 @@ setup(
     packages=find_packages(exclude=['tests', 'tests.*']),
     long_description=long_description,
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
-    setup_requires=[
-        'pytest-runner~=4.2',
-    ],
     install_requires=[
         'sensirion-i2c-driver~=1.0.0',
         'enum34;python_version<"3.4"',
